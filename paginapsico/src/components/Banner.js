fe816bc799d027578faf2796e0b073492532d60d
Jesus Rodriguez <jesus.jarr.30@gmail.com>
2023-05-02 16:57:35 -0600

    Animation react done and banner configuration

---
+++ paginapsico/src/components/Banner.js
@@ -0,0 +1,63 @@
+import { useState,useEffect } from "react";
+import { Container,Row, Col } from "react-bootstrap"
+import { ArrowRightCircle } from "react-bootstrap-icons"
+import headerIMG from "../assets/img/header-img.svg";
+import { DEFAULT_BREAKPOINTS } from "react-bootstrap/esm/ThemeProvider";
+
+export const Banner = () =>{
+    const[loopNum,setLoopNum]=useState(0);
+    const[isDeleting,setIsDeleting]= useState(false);
+    const toRotate = ["Web Developer","Web Desiner", "UI/UX Designer"];
+    const [text,setText]=useState('');
+    const period = 2000;
+    const [delta,setDelta] = useState(300-Math.random() * 100);
+
+    useEffect( () =>{
+        let ticker =setInterval(()=>{
+            tick();
+        },delta)
+        return () => {
+            clearInterval(ticker)
+        }
+    },[text])
+    const tick = () =>{
+        let i= loopNum %toRotate.length;
+        let fullText =toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1): fullText.substring(0,text.length+1)
+        setText(updatedText);
+        if(isDeleting){
+            setDelta(prevDelta => prevDelta /2)
+        }
+        if(!isDeleting && updatedText ===fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum +1);
+            setDelta(500);
+        }
+    }
+    return (
+        <section className="banner" id="home">
+
+            <Container>
+                <Row className="aling-items-center">
+                    <Col xs={12} md={6} xl={7}>
+
+                        <span className="tagling">Welcome to my Portafolio</span>
+                        <h1>{'Hi Im Jesus '}<span className="wrap">{text}</span></h1>
+                        <p>hello world</p>
+                        <button onClick={()=> console.log('conect')}>let´s coneect<ArrowRightCircle size={25}/></button>
+                        
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                    <img src={headerIMG} alt="Headder img"/>
+
+                    </Col>
+                </Row>
+
+            </Container>
+
+        </section>
+    )
+}
